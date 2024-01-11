a557b1c64b86af06dd2c84cb2ded6967d182ee17
Mike Zbrozek <mzbrozek@cnm.edu>
2024-01-11 07:17:23 -0700

    First commit

---
+++ SimpleCalc.java
@@ -0,0 +1,17 @@
+// Name: Mike Zbrozek
+// Class: 2251
+// Assignment: SimpleCalc
+// Purpose: Add two numbers
+// FileName: "SimpleCalc.java"
+
+public class SimpleCalc {
+
+    public static void main(String[] args) {
+        int num1 = 14;
+        int num2 = 104;
+
+        int result = num1 + num2;
+
+        System.out.println(result);
+    }
+}
